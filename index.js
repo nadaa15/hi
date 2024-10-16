f4fd03de354eec75e915dae3fddacf0b2be1d5c2
Your Name <na835126@gmail.com>
2024-10-16 21:36:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//! A function that takes an array of numbers and returns a new array containing only the even numbers.
+
+// let arr = [55, 107, 86, 31, 99, 42, 18, 120];
+// function getEvenNums(arr) {
+//     let arrOfEvenNums = arr.filter((num) => num % 2 == 0);
+//     console.log(arrOfEvenNums);
+// }
+// getEvenNums(arr)
+
+
+//! A function that takes an array of numbers and returns the largest number in the array.
+
+// let arr = [10, 4, 25, 13, 8, 19];
+// function grtLargestNum(arr) {
+//   let largestNum = Math.max(...arr)
+//   console.log(largestNum);
+// }
+// grtLargestNum(arr);
+
+
+//! A function that takes a string as input and returns the same string but reversed.
+
+// function reverseString(str) {
+//     let reversedStr = str.split("").reverse().join("")
+//   console.log(reversedStr);
+// }
+// reverseString("Hello")
+
+
+//! A function that takes an array of numbers and returns a new array without duplicate numbers.
+
+// let arr = [1, 5, 10, 1, 8, 6, 5, 11, 8, 3];
+// function removeDuplicates(arr) {
+//   let newArr = Array.from(new Set(arr))
+//   console.log(newArr);
+// }
+// removeDuplicates(arr)
